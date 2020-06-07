81136033e3127ed7033c8cc49b5971edd1ced688
Tintinnabulate <tintinn@abulate>
2020-06-07 09:32:20 +0100

    Add speaker tapes & resources link to sidebar

--- src/components/Textual.tsx
+++ src/components/Textual.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Text } from '@chakra-ui/core';
+import { Text, Stack } from '@chakra-ui/core';
 
 export function Textual() {
   return (
-    <Text>To update information here, send an email to <a href="mailto:thevengo@gmail.com">thevengo@gmail.com</a></Text>
+    <Stack>
+        <Text>To update information here, send an email to <a href="mailto:thevengo@gmail.com">thevengo@gmail.com</a></Text>
+        <Text>Click here to view a list of <a href="https://docs.google.com/spreadsheets/d/1mJsAtK9GYq2TJwFSKbkq9OD436s00E_pTx1Oj96HbjI/edit#gid=0">YPAA speaker tapes & resources </a>.</Text>
+    </Stack>
   );
 }
