67a62a4c31bcc42582e86c5b4b3c953dacebdfc4
Tintinnabulate <tintinn@abulate>
2021-01-27 20:12:56 +0000

    Shorten sidebar text

--- src/components/Textual.tsx
+++ src/components/Textual.tsx
@@ -5,9 +5,9 @@ export function Textual() {
   return (
     <Stack>
         <Text>All meeting times are in your timezone.</Text>
-        <Text>To update information here, send an email to <a href="mailto:thevengo@gmail.com">thevengo@gmail.com</a></Text>
-        <Text>Click here to view a list of <a href="https://docs.google.com/spreadsheets/d/1mJsAtK9GYq2TJwFSKbkq9OD436s00E_pTx1Oj96HbjI/edit#gid=0">YPAA speaker tapes/recordings & Online resources</a>.</Text>
-        <Text>Click here to view a gallery of <a href="https://photos.google.com/share/AF1QipP6wBEm-jbNvmQ6q5XlG4yLPLlgarfcOKV11_ucuvUuWzzgfyXfNuYXRGHKb0BVog?key=VC1mLTRwX0lBX3NPVG5hSnBDcmgtT2h4WFd6WGFB">flyers for upcoming YPAA Events and Conventions</a>.</Text>
+        <Text>To update information here, send an email to <a href="mailto:thevengo@gmail.com">thevengo@gmail.com</a>.</Text>
+        <Text><a href="https://docs.google.com/spreadsheets/d/1mJsAtK9GYq2TJwFSKbkq9OD436s00E_pTx1Oj96HbjI/edit#gid=0">YPAA share recordings</a>.</Text>
+        <Text><a href="https://photos.google.com/share/AF1QipP6wBEm-jbNvmQ6q5XlG4yLPLlgarfcOKV11_ucuvUuWzzgfyXfNuYXRGHKb0BVog?key=VC1mLTRwX0lBX3NPVG5hSnBDcmgtT2h4WFd6WGFB">YPAA Events and Conventions</a>.</Text>
     </Stack>
   );
 }
